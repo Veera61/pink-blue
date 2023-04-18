4b257b948ccaf3188f7e946a0f7bfeb787d37400
Veera61 <satya.kondreddi61@gmail.com>
2023-04-18 21:40:14 +0530

    Page open in new tab changes

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  clickMethod() {
+    const openInNewTab = confirm("Do you want open this in new tab");
+    if(openInNewTab){
+      window.open("https://www.angular.io", '_blank');
+    } else {
+      alert('user dont want to open this link in new page');
+    }
+  }
+}
